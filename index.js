8edfd78e3919dd22550cf1c3a473deb32973d8eb
KvRae <karamelmannai@gmail.com>
2023-02-10 23:02:40 +0100

    init api

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express();
 const port = 3000;
 
 
-app.get("/"),(req,res)=> res.send("Stripe Api Service")
-app.listen
+app.get('/', (req, res) => res.send('Stripe Payment Service!'));
+app.listen(port, () => console.log(`Stripe Payment Service listening on port http://localhost:${port} !`))
